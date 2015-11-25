7892f7dac0023c17cc6ff782faac4df21e77844f
ives_markus <markus.wallisch@interactives.eu>
2015-11-25 23:39:38 +0100

    Use Bower and Gulp to pull in jquery,bootstrap,fontawesome and setup

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+
+var bowerPath = "./bower_components";
+var jsPath = "./js/vendor";
+var cssPath = "./css/vendor";
+var fontPath = "./fonts/vendor";
+
+gulp.task('init', function() {
+    // jquery
+    console.log('copy jquery');
+    gulp.src(bowerPath+"/jquery/dist/jquery*")
+    .pipe(gulp.dest(jsPath));
+
+    // bootstrap
+    console.log('copy bootstrap js');
+    gulp.src(bowerPath+"/bootstrap/dist/js/bootstrap*")
+    .pipe(gulp.dest(jsPath));
+
+    console.log('copy bootstrap css');
+    gulp.src(bowerPath+"/bootstrap/dist/css/*")
+    .pipe(gulp.dest(cssPath));
+
+    console.log('copy bootstrap fonts (glyphicons)')
+    gulp.src(bowerPath+"/bootstrap/dist/fonts/*")
+    .pipe(gulp.dest(fontPath));
+
+    // fontawesome
+    console.log('copy fontawesome css')
+    gulp.src(bowerPath+"/font-awesome/css/*")
+    .pipe(gulp.dest(cssPath));
+
+    console.log('copy fontawesome fonts')
+    gulp.src(bowerPath+"/font-awesome/fonts/*")
+    .pipe(gulp.dest(fontPath));
+});
+
+gulp.task('default', function() {
+    console.log('default');
+});
